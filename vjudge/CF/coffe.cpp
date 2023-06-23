993c725d954c609e2d5f89f7491a184ac452a735
Noodle96 <jorgealfredomarino2@gmail.com>
2023-06-23 15:23:40 -0500

    coffee

---
+++ vjudge/CF/coffe.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define all(x) x.begin(), x.end()
+#define HERE cout << "i here" << endl;
+using namespace std;
+void solve(){
+	string str; cin>>str;
+	if(str[2] == str[3] && str[4] == str[5]){
+		cout << "Yes" << endl; return;
+	}cout << "No" << endl;
+}
+int main(){
+	solve();
+	return 0;
+}

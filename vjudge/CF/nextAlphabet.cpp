f05c09c868bc64310631fcfecfe36ef063ea507c
Noodle96 <jorgealfredomarino2@gmail.com>
2023-06-23 15:14:28 -0500

    next alphabet

---
+++ vjudge/CF/nextAlphabet.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#define all(x) x.begin(), x.end()
+#define HERE cout << "i here" << endl;
+using namespace std;
+int main(){
+	char ch; cin>>ch;
+	string str ; str+= (ch +1);
+	cout << str << endl;
+	return 0;
+}

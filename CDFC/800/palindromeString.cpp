90e3dac364510c758b4b76bec8911f58f9d1e123
Noodle96 <jorgealfredomarino2@gmail.com>
2023-07-08 10:54:46 -0500

    LuoTianyi and the Palindrome String, greedy string

---
+++ CDFC/800/palindromeString.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#define all(x) x.begin(),x.end()
+#define msg(str,str2) cout << str << str2<< endl
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+void solve(){
+	string s; cin>> s;
+	map<char,int> mapa;
+	for(int e = 0 ;e < s.size() ; e++) mapa[s[e]]++;
+	if(mapa.size() == 1 ){
+		cout << -1 << endl; return;
+	}
+	cout << s.size()-1 << endl;
+}
+int main(){
+	// #ifndef IN_OUT
+	// 	freopen("input.txt","r",stdin);
+	// 	freopen("output.txt","w",stdout);
+	// #endif
+	
+	int t; cin>>t;
+	while(t--){
+		solve();
+	}
+	
+	return 0;
+}

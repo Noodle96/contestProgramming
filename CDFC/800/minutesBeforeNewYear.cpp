0c5dbf40496e0cc3c7946e6d3da749e3f17dcd7e
Noodle96 <jorgealfredomarino2@gmail.com>
2023-07-10 13:33:12 -0500

    1283A minutes before new year math 800

---
+++ CDFC/800/minutesBeforeNewYear.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define all(x) x.begin(),x.end()
+#define msg(str,str2) cout << str << str2<< endl
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+void solve(){
+	int h,m; cin>>h>>m;
+	cout << (23-h)*60 + (60-m) << endl;
+}
+int main(){
+	// #ifndef IN_OUT
+	// 	freopen("input.txt","r",stdin);
+	// 	freopen("output.txt","w",stdout);
+	// #endif
+	
+	int t; cin>>t;
+	while(t--){
+		solve();
+	}
+	
+	return 0;
+}

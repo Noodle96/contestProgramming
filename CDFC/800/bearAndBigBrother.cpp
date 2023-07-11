c2b9d9387e3558be915570ac47e6029b3123aa43
Noodle96 <jorgealfredomarino2@gmail.com>
2023-07-11 09:34:34 -0500

    bear and big brother implementation 800

---
+++ CDFC/800/bearAndBigBrother.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define all(x) x.begin(),x.end()
+#define msg(str,str2) cout << str << str2<< endl
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+
+void solve(){
+	int a,b; cin>>a>>b;
+	int cont = 0 ;
+	while(b >= a){
+		a *=3;
+		b*=2;
+		cont++;
+	}
+	cout << cont << endl;
+}
+int main(){
+	solve();
+	return 0;
+}

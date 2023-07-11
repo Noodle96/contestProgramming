f3afd459e35fa70abab707afec38d66903476ee2
Noodle96 <jorgealfredomarino2@gmail.com>
2023-07-11 10:16:51 -0500

    subtraction  Games

---
+++ CDFC/CDFC-D1+D2/subtractionGame.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#define all(x) x.begin(),x.end()
+#define msg(str,str2) cout << str << str2<< endl
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+void solve(){
+	int a,b; cin>>a>>b; //a < b
+	if(a > 1){
+		cout << a - 1 << endl; return;
+	}
+	if(2*a < b){
+		cout << 2*a << endl;return;
+	}
+	cout << 3 << endl;
+}
+int main(){
+	// #ifndef IN_OUT
+	// 	freopen("input.txt","r",stdin);
+	// 	freopen("output.txt","w",stdout);
+	// #endif
+	
+	int t; cin>>t;
+	while(t--){
+		solve();
+	}
+	
+	return 0;
+}

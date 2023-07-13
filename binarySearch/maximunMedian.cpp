a5a5769f175e8bad3cc5c0e64dfe33a2b5ded47d
Noodle96 <jorgealfredomarino2@gmail.com>
2023-07-13 13:34:14 -0500

    clear folder

--- binarySearch/maximunMedian.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-int main() {
-    unsigned int maximo = 2000000000;
-    std::cout << INT_MAX << std::endl;
-    cout << UINT_MAX << endl;
-    
-    return 0;
-}

f403e8d1c44760cf04bc89ae2fe142cb1d1fae4a
Noodle96 <jorgealfredomarino2@gmail.com>
2023-07-14 18:38:47 -0500

    1554A Chery greedy 800

---
+++ CDFC/800/cherry.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#define all(x) x.begin(),x.end()
+#define msg(str,str2) cout << str << str2<< endl
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+void solve(){
+	int n;cin>>n;
+	vector<int> vec(n);
+	for(auto &e:vec) cin>>e;
+	ll rpta = 0;
+	for(int e = 1 ;e < n ; e++) rpta = max(rpta, 1LL*vec[e]*vec[e-1]);
+	cout << rpta<< endl;
+}
+int main(){
+	// #ifndef IN_OUT
+	// 	freopen("input.txt","r",stdin);
+	// 	freopen("output.txt","w",stdout);
+	// #endif
+	
+	int t; cin>>t;
+	while(t--){
+		solve();
+	}	
+	return 0;
+}

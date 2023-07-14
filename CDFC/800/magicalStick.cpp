b2d9d2f524539134671d3ae89dc877d708d766c3
Noodle96 <jorgealfredomarino2@gmail.com>
2023-07-14 17:40:33 -0500

    1371A Magical stick math *800

---
+++ CDFC/800/magicalStick.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define all(x) x.begin(),x.end()
+#define msg(str,str2) cout << str << str2<< endl
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+void solve(){
+	int n; cin>>n;
+	if(n%2 == 0) cout << n/2 << endl;
+	else cout << n/2 +1 << endl;
+}
+int main(){
+	int t; cin>>t;
+	while(t--){
+		solve();
+	}
+	
+	return 0;
+}

16386983e3f2c8435e100246498b262b291e3014
Noodle96 <jorgealfredomarino2@gmail.com>
2023-07-14 16:26:54 -0500

    630A Again twenty five math *800

---
+++ CDFC/800/again25.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define all(x) x.begin(),x.end()
+#define msg(str,str2) cout << str << str2<< endl
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+void solve(){
+	int n; cin>>n;
+	cout << 25 << endl;
+}
+int main(){
+	solve();
+	return 0;
+}

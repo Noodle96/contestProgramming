c5d9cff9a42a1b79342fee3b927de3f7450da204
Noodle96 <jorgealfredomarino2@gmail.com>
2023-07-15 09:58:02 -0500

    getCloser

---
+++ vjudge/CF/getCloser.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define all(x) x.begin(),x.end()
+#define msg(str,str2) cout << str << str2<< endl
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+void solve(){
+	double a,b; cin>>a>>b;
+	double distance = sqrt(a*a+b*b);
+	cout << double(a/distance) << " " << double(b/distance) << endl;
+}
+int main(){
+	solve();
+	return 0;
+}

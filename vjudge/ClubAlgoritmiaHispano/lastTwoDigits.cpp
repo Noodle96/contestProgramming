52eddca7915295f6c0f8c5e3edf48d2f85703ade
Noodle96 <jorgealfredomarino2@gmail.com>
2023-07-18 18:02:58 -0500

    last Two Digits

---
+++ vjudge/ClubAlgoritmiaHispano/lastTwoDigits.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define all(x) x.begin(),x.end()
+#define msg(str,str2) cout << str << str2<< endl
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+void solve(){
+	string s; cin>>s;
+	cout << s[s.size()-2] << s[s.size()-1] << endl;
+}
+int main(){
+	solve();
+	return 0;
+}

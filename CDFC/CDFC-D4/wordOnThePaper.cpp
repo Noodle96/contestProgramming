8cec1b5b285f3843e10d1e934408d52dc4355baf
Noodle96 <jorgealfredomarino2@gmail.com>
2023-07-21 13:55:51 -0500

    wordOnThePaper

---
+++ CDFC/CDFC-D4/wordOnThePaper.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#define all(x) x.begin(),x.end()
+#define msg(str,str2) cout << str << str2<< endl
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+void solve(){
+	string line,ans="";
+	for(int e = 0 ; e < 8; e++){
+		cin>>line;
+		// cout << "line: " <<line << endl;
+		for(int j = 0 ;j < 8 ; j++){
+			if(line[j] != '.') ans += line[j];
+		}
+	}
+	cout << ans << endl;
+}
+int main(){
+	// #ifndef IN_OUT
+	// 	freopen("input.txt","r",stdin);
+	// 	freopen("output.txt","w",stdout);
+	// #endif
+	
+	int t; cin>>t;
+	while(t--){
+		solve();
+	}
+	
+	return 0;
+}

3e820576ace2f9cb6728999ceb3dbf6997df02ba
Noodle96 <jorgealfredomarino2@gmail.com>
2023-07-22 09:05:49 -0500

    ascci code

---
+++ vjudge/UAGRM/G_ascciCode.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define all(x) x.begin(),x.end()
+#define msg(str,str2) cout << str << str2<< endl
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+void solve(){
+	int n; cin>>n;
+	char c = '\0';
+	cout << char(c + n) << endl;
+}
+int main(){
+	solve();
+	return 0;
+}

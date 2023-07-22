ed01c4a0e2e8edd901358395ec9dc2a530455d5e
Noodle96 <jorgealfredomarino2@gmail.com>
2023-07-22 09:05:37 -0500

    Six characters

---
+++ vjudge/UAGRM/D-sixCharacters.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define all(x) x.begin(),x.end()
+#define msg(str,str2) cout << str << str2<< endl
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+void solve(){
+	string s; cin>>s;
+	//abc
+	for(int e = 0 ;e < 6 ; e++){
+		cout << s[e%s.size()];
+	}cout<< endl;
+}
+int main(){
+	solve();
+	return 0;
+}

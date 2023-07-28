21a3efa6ffd6b39202b24a43c3876e68b40ea662
Noodle96 <jorgealfredomarino2@gmail.com>
2023-07-28 14:21:15 -0500

    520A pangram string implementation *800

---
+++ CDFC/800/pangram.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define all(x) x.begin(),x.end()
+#define msg(str,str2) cout << str << str2<< endl
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+
+#define pb push_back
+#define mp make_pair
+void solve(){
+	int n; cin>>n;
+	map<char, int> map;
+	char c;
+	for(int e = 0 ; e < n; e++){
+		cin>>c;
+		c = tolower(c);
+		map[c]++;
+	}
+	if(map.size() == 26) cout << "YES" << endl;
+	else cout << "NO" << endl;
+}
+int main(){
+	solve();
+	return 0;
+}

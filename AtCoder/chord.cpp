56d0f5681847d84009c2fda9a1bf5486324d8a7e
Noodle96 <jorgealfredomarino2@gmail.com>
2023-07-29 07:38:37 -0500

    chord

---
+++ AtCoder/chord.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define all(x) x.begin(),x.end()
+#define msg(str,str2) cout << str << str2<< endl
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+
+#define pb push_back
+#define mp make_pair
+
+void solve(){
+	string s; cin>>s;
+	if(s == "ACE" || s== "BDF" || s == "CEG" || s == "DFA" || s==  "EGB" || s == "FAC" ||s == "GBD")cout << "Yes\n";
+	else cout << "No\n";
+}
+int main(){
+	solve();
+	return 0;
+}

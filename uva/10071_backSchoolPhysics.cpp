91a9d00105dcf722850a74382d79d5df7fab594c
Noodle96 <jorgealfredomarino2@gmail.com>
2023-07-31 13:50:47 -0500

    back_school_physics

---
+++ uva/10071_backSchoolPhysics.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define all(x) x.begin(),x.end()
+#define msg(str,str2) cout << str << str2<< endl
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+
+#define pb push_back
+#define mp make_pair
+void solve(){
+	int v,t;
+	while(cin>>v>>t){
+		cout << 2*v*t << endl;
+	}
+}
+
+
+
+int main(){
+	solve();
+	return 0;
+}

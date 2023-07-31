12c57cb318a9a6419be4f13acac7d0ab344ad308
Noodle96 <jorgealfredomarino2@gmail.com>
2023-07-31 14:30:22 -0500

    13025_back to the past

---
+++ uva/13025_backToThePast.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define all(x) x.begin(),x.end()
+#define msg(str,str2) cout << str << str2<< endl
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+
+#define pb push_back
+#define mp make_pair
+
+int main(){
+	cout << "May 29, 2013 Wednesday" << endl;
+	return 0;
+}

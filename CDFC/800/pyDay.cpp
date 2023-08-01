22b7d3de17aa4cb25a4d52144d0f120d2631547f
Noodle96 <jorgealfredomarino2@gmail.com>
2023-08-01 11:24:09 -0500

    Polycarp and pi  day math string *800

---
+++ CDFC/800/pyDay.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#define all(x) x.begin(),x.end()
+#define msg(str,str2) cout << str << str2<< endl
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+
+#define pb push_back
+#define mp make_pair
+
+const string pi = "314159265358979323846264338327";
+
+void solve(){
+	string str; cin>>str;
+	int cont = 0;
+	for(int e = 0 ;e < str.size() ; e++){
+		if(str[e] == pi[e]) cont++;
+		else break;
+	}
+	cout << cont << endl;
+}
+int main(){
+	// #ifndef IN_OUT
+	// 	freopen("input.txt","r",stdin);
+	// 	freopen("output.txt","w",stdout);
+	// #endif
+	
+	int t; cin>>t;
+	while(t--){
+		solve();
+	}
+	
+	return 0;
+}

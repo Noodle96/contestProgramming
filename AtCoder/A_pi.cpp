d7e4386b154f211fe9179acb09c1b54f533f9540
Noodle96 <jorgealfredomarino2@gmail.com>
2023-08-12 07:23:20 -0500

    pi

---
+++ AtCoder/A_pi.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define all(x) x.begin(),x.end()
+#define msg(str,str2) cout << str << str2<< endl
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+
+#define pb push_back
+#define mp make_pair
+
+const string pi = "3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679";
+void solve(){
+	int n; cin>>n;
+	for(int e = 0 ;e < n+2 ; e++){
+		cout << pi[e];
+	}cout << endl;
+}
+int main(){
+	solve();
+	return 0;
+}

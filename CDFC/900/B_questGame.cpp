57063a1a56c750802a25f6d0b979c235241877df
Noodle96 <jorgealfredomarino2@gmail.com>
2023-10-01 23:44:39 -0500

    B_questGame greedy *900 math

---
+++ CDFC/900/B_questGame.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#define all(x) x.begin(),x.end()
+#define msg(str,str2) cout << str << str2<< endl
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+
+#define pb push_back
+#define mp make_pair
+
+clock_t startTime;
+double getCurrentTime() {
+	return (double)(clock() - startTime) / CLOCKS_PER_SEC;
+}
+void solve(){
+	int x,n,m; cin>>x>>n>>m;
+	if(10*m >= x){
+		cout << "YES" << endl;return;
+	}
+	while(n--){
+		x = x/2 +10;
+	}
+	x = x-10*m;
+	if(x <= 0){
+		cout << "YES" << endl;
+	}else cout << "NO" << endl;
+}
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	// #ifndef IN_OUT
+	// 	freopen("input.txt","r",stdin);
+	// 	freopen("output.txt","w",stdout);
+	// #endif
+	
+	int t; cin>>t;
+	while(t--){
+		solve();
+	}
+	return 0;
+}

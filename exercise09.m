36b5a37fa970b12691fa674d223a69c267dc1439
Noodle96 <jorgealfredomarino2@gmail.com>
2023-10-15 16:46:31 -0500

    Refactoring

--- exercise09.m
+++
@@ -1,8 +0,0 @@
-function ans = exercise09(a,n,e)
-  x = a;
-  while(abs(power(x,n) -a ) > e) # controlando la magnitid del error absouto
-    x = x - ((power(x,n) -a)) / (n*power(x,n-1));
-    disp(x);
-  endwhile
-  ans = x;
-endfunction

4505ae797fde7986e3664de250389051d3b54975
Noodle96 <jorgealfredomarino2@gmail.com>
2023-10-24 12:19:54 -0500

    B_sequentialMin games *1100

---
+++ CDFC/1100/B_sequentialMin.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+#define all(x) x.begin(),x.end()
+#define msg(str,str2) cout << str << str2<< endl
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+
+#define pb push_back
+#define mp make_pair
+
+int gcd(int a,int b){
+	if(a%b==0) return b;
+	else return gcd(b,a%b);
+}
+
+clock_t startTime;
+double getCurrentTime() {
+	return (double)(clock() - startTime) / CLOCKS_PER_SEC;
+}
+const int N = 1e5 +5;
+int ar[N];
+int n;
+void solve(){
+	cin>>n;
+	for(int e = 0 ; e < n; e++) cin>> ar[e];
+	int k = 0;
+	while(k<n && ar[k] == 1) k++;
+	//all ones
+	if(k == n){
+		if(k%2==0)cout << "Second" << endl;
+		else  cout << "First" << endl;
+	}else{
+		if(k%2==0) cout << "First" << endl;
+		else cout << "Second" << endl;
+	}
+}
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	int t; cin>>t;
+	while(t--){
+		solve();
+	}
+	
+	return 0;
+}

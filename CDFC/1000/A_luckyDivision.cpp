94e889088804919460c77ae1118003761b4cdac3
Noodle96 <jorgealfredomarino2@gmail.com>
2023-11-15 18:48:47 -0500

    122A - Lucky Division

---
+++ CDFC/1000/A_luckyDivision.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#define all(x) x.begin(),x.end()
+#define msg(str,str2) cout << str << str2<< endl
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+
+#define pb push_back
+#define mp make_pair
+
+int gcd(int a,int b){
+	if(a%b==0) return b;
+	else return gcd(b,a%b);
+}
+
+clock_t startTime;
+double getCurrentTime() {
+	return (double)(clock() - startTime) / CLOCKS_PER_SEC;
+}
+void solve(){
+	int n; cin>>n;
+	if(n%4==0 || n%7 ==0 || n%44==0 || n%47==0 || n%74==0 || n%77==0 ||
+		n%444==0 || n%447==0 || n%474==0 || n%477 == 0 || n% 744==0||
+		n%747==0 || n%774==0 || n%777 ==0){
+			cout << "YES" << endl;
+		}else{
+			cout << "NO" << endl;
+		}
+}
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	#ifdef DEBUG
+		freopen("input.txt","r",stdin);
+		freopen("output.txt","w",stdout);
+	#endif
+	solve();
+	return 0;
+}

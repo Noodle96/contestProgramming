e16f9aaf048e9673a3c9fac77fbe61d98bf11c79
Noodle96 <jorgealfredomarino2@gmail.com>
2023-11-15 14:03:05 -0500

    factorial

---
+++ Template/DP/factorial.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#define all(x) x.begin(),x.end()
+#define msg(str,str2) cout << str << str2<< endl
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+
+#define pb push_back
+#define mp make_pair
+
+int gcd(int a,int b){
+	if(a%b==0) return b;
+	else return gcd(b,a%b);
+}
+
+clock_t startTime;
+double getCurrentTime() {
+	return (double)(clock() - startTime) / CLOCKS_PER_SEC;
+}
+
+ll factorialIterativo(int n){
+	ll ans = 1;
+	for(int e = n; e > 0; e--) ans *= e;
+	return ans;
+}
+
+ll factorialRecursivo(int n){
+	if(n == 0) return 1;
+	return n*factorialIterativo(n-1);
+}
+
+void solve(){
+	int n; cin>>n;
+	// cout << factorialIterativo(n) << endl;
+	cout << factorialRecursivo(n) << endl;
+}
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	solve();
+	return 0;
+}

24e00f836e287371f4e8217f7507d831febcc2c3
Jorge Tito <jorgealfredomarino2@gmail.com>
2023-12-09 18:36:38 -0500

    11044	Searching for Nessy

---
+++ uva/RepetitionOnly/11044_Searching_for_Nessy.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#define all(x) x.begin(),x.end()
+#define msg(str,str2) cout << str << str2<< endl
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+
+#define pb push_back
+#define mp make_pair
+
+int gcd(int a,int b){
+	if(a%b==0) return b;
+	else return gcd(b,a%b);
+}
+
+clock_t startTime;
+double getCurrentTime() {
+	return (double)(clock() - startTime) / CLOCKS_PER_SEC;
+}
+void solve(){
+	int n,m;
+	cin>>n>>m;
+	n-=2; m-=2;
+	int a = (n%3==0?n/3:n/3+1);
+	int b = (m%3==0?m/3:m/3+1);
+	cout << a*b << endl;
+}
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	#ifdef DEBUG
+		freopen("input.txt","r",stdin);
+		freopen("output.txt","w",stdout);
+	#endif
+	int t; cin>>t;
+	while(t--){
+		solve();
+	}
+	
+	return 0;
+}

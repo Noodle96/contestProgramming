ef38f5e8fbe953f7ee45461e4128d39b502945e6
Jorge Tito <jorgealfredomarino2@gmail.com>
2023-12-19 10:45:43 -0500

    B - Preparing for the Contest

---
+++ CDFC/CDFC-D3/B_Preparing_for_the_Contest.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+#define all(x) x.begin(),x.end()
+#define msg(str,str2) cout << str << str2<< endl
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+
+#define pb push_back
+#define mp make_pair
+
+int gcd(int a,int b){
+	if(a%b==0) return b;
+	else return gcd(b,a%b);
+}
+
+clock_t startTime;
+double getCurrentTime() {
+	return (double)(clock() - startTime) / CLOCKS_PER_SEC;
+}
+void solve(){
+	int n, k; cin>>n>>k;
+	int init = n-k;
+	int init_ = init;
+	for(int e = 0 ; e < k+1 ; e++){
+		cout << init++ << " ";
+	}
+	
+	for(int e = init_-1; e > 0; e--){
+		cout << e << " ";
+	}cout << endl;
+
+}
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	#ifdef DEBUG
+		freopen("input.txt","r",stdin);
+		freopen("output.txt","w",stdout);
+	#endif
+	int t; cin>>t;
+	while(t--){
+		solve();
+	}
+	
+	return 0;
+}

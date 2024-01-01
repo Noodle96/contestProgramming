e247a4d60d4aac5121816c5a31ba811df6dfde01
Jorge Tito <jorgealfredomarino2@gmail.com>
2024-01-01 12:55:01 -0500

    B - Buy One Carton of Milk

--- AtCoder/B_Buy_One_CartonOfMilk.cpp
+++ AtCoder/B_Buy_One_CartonOfMilk.cpp
@@ -25,30 +25,22 @@ clock_t startTime;
 double getCurrentTime() {
 	return (double)(clock() - startTime) / CLOCKS_PER_SEC;
 }
-int ar[3];
-int precio[3]={6,8,12};
-void solve(){
-	int ans = INT_MAX;
-	int N;cin>>N;
-	for(int e = 0 ; e < 3; e++)cin>>ar[e];
-	for(int e = 0 ; e < 3; e++){
-		int resto = N%precio[e];
-		if(resto == 0){
-			ans = min(ans,(N/precio[e])*ar[e]);
-		}else{
-			if(resto == 6 || resto == 8 || resto == 12){
-				int r = N - N/precio[e];
-				int temp = (N/precio[e])*ar[e] + r;
-				ans = min(ans, temp);
-			}
-		}
-	}if(ans == INT_MAX){
-		for(int e = 0 ;e < 3  ; e++){
-			ans = min(ans, (N/precio[e] +1 )*ar[e]);
-		}
-	}
-	cout << ans << endl;
 
+vector<int> h{6,8,12};
+vector<int> p(3);
+int ans = INT_MAX;
+
+int f(int n, int T){
+	if(n<=0) return T;
+	for(int e = 0 ; e < 3; e++){
+		ans = min(ans, f(n-h[e], T+p[e]));
+	}
+	return ans;
+}
+void solve(){
+	int n;cin>>n;
+	for(int e= 0; e < 3 ; e++) cin>>p[e];
+	cout << f(n,0) << endl;
 }
 int main(){
 	ios_base::sync_with_stdio(false);

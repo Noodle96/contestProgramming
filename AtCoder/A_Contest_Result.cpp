321135fc5092833471399f2d4d6363dd83c78656
Noodle96 <jorgealfredomarino2@gmail.com>
2024-01-19 18:00:53 -0500

    A - Contest Result

---
+++ AtCoder/A_Contest_Result.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#define all(x) x.begin(),x.end()
+#define msg(str,str2) cout << str << str2<< endl
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+
+#define pb push_back
+#define mp make_pair
+
+int gcd(int a,int b){
+	if(a%b==0) return b;
+	else return gcd(b,a%b);
+}
+
+clock_t startTime;
+double getCurrentTime() {
+	return (double)(clock() - startTime) / CLOCKS_PER_SEC;
+}
+const int N = 100;
+int ar[N];
+void solve(){
+	int n,m; cin>>n>>m;
+	for(int e = 0 ; e < n; e++) cin>>ar[e];
+	int ans = 0,a ;
+	for(int e = 0 ;  e < m;e++){
+		cin>>a;
+		ans += ar[a-1];
+	}
+	cout << ans << endl;
+}
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	#ifdef DEBUG
+		freopen("input.txt","r",stdin);
+		freopen("output.txt","w",stdout);
+	#endif
+	solve();
+	return 0;
+}

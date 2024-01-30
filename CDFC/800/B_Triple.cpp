077183033fec25670b476873c5493f59e56c1688
Noodle96 <jorgealfredomarino2@gmail.com>
2024-01-29 20:44:37 -0500

    B - Triple

---
+++ CDFC/800/B_Triple.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+#define all(x) x.begin(),x.end()
+#define msg(str,str2) cout << str << str2<< endl
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+
+#define pb push_back
+#define mp make_pair
+
+int gcd(int a,int b){
+	if(a%b==0) return b;
+	else return gcd(b,a%b);
+}
+
+clock_t startTime;
+double getCurrentTime() {
+	return (double)(clock() - startTime) / CLOCKS_PER_SEC;
+}
+void solve(){
+	int n; cin>>n;
+	map<int,int> map;
+	for(int e = 0 ,temp;e < n ; e++){
+		cin>>temp;
+		map[temp]++;
+	}
+	for(auto e: map){
+		if(e.second >=3){
+			cout << e.first << endl;
+			return;
+		}
+	}
+	cout << -1 << endl;
+}
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	#ifdef DEBUG
+		freopen("input.txt","r",stdin);
+		freopen("output.txt","w",stdout);
+	#endif
+	int t; cin>>t;
+	while(t--){
+		solve();
+	}
+	
+	return 0;
+}

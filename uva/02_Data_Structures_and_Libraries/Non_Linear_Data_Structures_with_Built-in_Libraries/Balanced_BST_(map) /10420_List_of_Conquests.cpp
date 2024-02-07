5a6e8f042d32d78bdc20600390b902afa0a756da
Noodle96 <jorgealfredomarino2@gmail.com>
2024-02-07 10:34:40 -0500

    10420	List of Conquests

---
+++ uva/02_Data_Structures_and_Libraries/Non_Linear_Data_Structures_with_Built-in_Libraries/Balanced_BST_(map) /10420_List_of_Conquests.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+#define all(x) x.begin(),x.end()
+#define msg(str,str2) cout << str << str2<< endl
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+
+#define pb push_back
+#define mp make_pair
+
+int gcd(int a,int b){
+	if(a%b==0) return b;
+	else return gcd(b,a%b);
+}
+
+clock_t startTime;
+double getCurrentTime() {
+	return (double)(clock() - startTime) / CLOCKS_PER_SEC;
+}
+void solve(){
+	int n; cin>>n;
+	map<string , int> m;
+	cin.ignore();
+	while(n--){
+		string s;
+		getline(cin,s);
+		stringstream ss(s);
+		string name;
+		ss>>name;
+		m[name]++;
+	}
+	for(auto e: m){
+		cout << e.first << " " << e.second << endl;
+	}
+}
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	#ifdef DEBUG
+		freopen("input.txt","r",stdin);
+		freopen("output.txt","w",stdout);
+	#endif
+	solve();
+	return 0;
+}

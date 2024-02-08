4aaa6ebeb02559298b56ac2fde6eba6f388a06c6
Noodle96 <jorgealfredomarino2@gmail.com>
2024-02-08 13:43:36 -0500

    10107	What is the Median?

---
+++ uva/02_Data_Structures_and_Libraries/Linear_Data_Structures_with_Built-in_Libraries/Sorting_Easier/10107_What_is_the_Median.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+#define all(x) x.begin(),x.end()
+#define msg(str,str2) cout << str << str2<< endl
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+
+#define pb push_back
+#define mp make_pair
+
+int gcd(int a,int b){
+	if(a%b==0) return b;
+	else return gcd(b,a%b);
+}
+
+clock_t startTime;
+double getCurrentTime() {
+	return (double)(clock() - startTime) / CLOCKS_PER_SEC;
+}
+ll a;
+vector<ll> vec;
+void solve(){
+	while(cin>>a){
+		vec.pb(a);
+		sort(all(vec));
+		ll m = (int)vec.size();
+		if( m & 1){
+			cout << vec[m/2] << endl;
+		}else{
+			cout << (vec[m/2] + vec[m/2-1])/2 << endl;
+		}
+	}
+}
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	#ifdef DEBUG
+		freopen("input.txt","r",stdin);
+		freopen("output.txt","w",stdout);
+	#endif
+	solve();
+	return 0;
+}

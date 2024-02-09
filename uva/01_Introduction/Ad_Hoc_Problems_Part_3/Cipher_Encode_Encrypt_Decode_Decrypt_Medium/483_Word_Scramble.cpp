65b6272180b69bd8a49b9fdc9f3827605c16c1cc
Noodle96 <jorgealfredomarino2@gmail.com>
2024-02-09 08:29:11 -0500

    483	Word Scramble

---
+++ uva/01_Introduction/Ad_Hoc_Problems_Part_3/Cipher_Encode_Encrypt_Decode_Decrypt_Medium/483_Word_Scramble.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+#define all(x) x.begin(),x.end()
+#define msg(str,str2) cout << str << str2<< endl
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+
+#define pb push_back
+#define mp make_pair
+
+int gcd(int a,int b){
+	if(a%b==0) return b;
+	else return gcd(b,a%b);
+}
+
+clock_t startTime;
+double getCurrentTime() {
+	return (double)(clock() - startTime) / CLOCKS_PER_SEC;
+}
+string str;
+void solve(){
+	while(getline(cin,str)){
+		stringstream ss(str);
+		string word;
+		vector<string> vec;
+		while(ss >> word){
+			vec.pb(word);
+		}
+		int e = 0;
+		for(;e < vec.size()-1;e++){
+			reverse(all(vec[e]));
+			cout << vec[e] << " ";
+		}
+		reverse(all(vec[e]));
+		cout << vec[e] << endl;
+	}
+}
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	#ifdef DEBUG
+		freopen("input.txt","r",stdin);
+		freopen("output.txt","w",stdout);
+	#endif
+	solve();
+	return 0;
+}

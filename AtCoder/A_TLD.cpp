cc7911fed6af264eca0d5af28d174960d11c13a5
Noodle96 <jorgealfredomarino2@gmail.com>
2024-02-26 15:56:39 -0500

    A_TLD

---
+++ AtCoder/A_TLD.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#define all(x) x.begin(),x.end()
+#define msg(str,str2) cout << str << str2<< endl
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+
+#define pb push_back
+#define mp make_pair
+
+int gcd(int a,int b){
+	if(a%b==0) return b;
+	else return gcd(b,a%b);
+}
+
+clock_t startTime;
+double getCurrentTime() {
+	return (double)(clock() - startTime) / CLOCKS_PER_SEC;
+}
+string s,ans; 
+void solve(){
+	cin>>s;
+	for(int e = (int)s.size()-1 ; e >= 0; e--){
+		if(s[e] != '.') ans += s[e];
+		else break;
+	}
+	reverse(all(ans));
+	cout << ans << endl;
+}
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	#ifdef DEBUG
+		freopen("input.txt","r",stdin);
+		freopen("output.txt","w",stdout);
+	#endif
+	solve();
+	return 0;
+}

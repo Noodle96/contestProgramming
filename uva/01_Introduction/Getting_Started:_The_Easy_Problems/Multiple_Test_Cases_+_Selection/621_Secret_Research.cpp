9b5b9551ead63a6adf0661def91911f2328e120b
Noodle96 <jorgealfredomarino2@gmail.com>
2024-02-28 16:58:31 -0500

    621 - Secret Research

---
+++ uva/01_Introduction/Getting_Started:_The_Easy_Problems/Multiple_Test_Cases_+_Selection/621_Secret_Research.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#define all(x) x.begin(),x.end()
+#define msg(str,str2) cout << str << str2<< endl
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+
+#define pb push_back
+#define mp make_pair
+
+int gcd(int a,int b){
+	if(a%b==0) return b;
+	else return gcd(b,a%b);
+}
+
+clock_t startTime;
+double getCurrentTime() {
+	return (double)(clock() - startTime) / CLOCKS_PER_SEC;
+}
+void solve(){
+	string s; cin>>s;
+	int n = (int)s.size();
+	if(s == "1" || s == "4" || s == "78") cout << "+" << endl;
+	else if(s[n-1] == '5' && s[n-2]=='3') cout << "-" << endl;
+	else if(s[0] == '9' && s[n-1] == '4') cout << "*" << endl;
+	else cout << "?" << endl;
+}
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	#ifdef DEBUG
+		freopen("input.txt","r",stdin);
+		freopen("output.txt","w",stdout);
+	#endif
+	int t; cin>>t;
+	while(t--){
+		solve();
+	}
+	
+	return 0;
+}

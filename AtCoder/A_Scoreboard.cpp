cee50659c49359bbfe335e357980a7c2e6077a57
Noodle96 <jorgealfredomarino2@gmail.com>
2024-03-05 18:16:48 -0500

    A - Scoreboard

---
+++ AtCoder/A_Scoreboard.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#define all(x) x.begin(),x.end()
+#define msg(str,str2) cout << str << str2<< endl
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+
+#define pb push_back
+#define mp make_pair
+
+int gcd(int a,int b){
+	if(a%b==0) return b;
+	else return gcd(b,a%b);
+}
+
+clock_t startTime;
+double getCurrentTime() {
+	return (double)(clock() - startTime) / CLOCKS_PER_SEC;
+}
+int n,t,a,temp;
+void solve(){
+	cin>>n;
+	t = a = 0;
+	for(int e = 0 ; e < n;e++){
+		cin>>temp;
+		t += temp;
+		cin>>temp;
+		a += temp;
+	}
+	cout << (t > a ? "Takahashi": (a > t ? "Aoki":"Draw")) << endl;
+}
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	#ifdef DEBUG
+		freopen("input.txt","r",stdin);
+		freopen("output.txt","w",stdout);
+	#endif
+	solve();
+	return 0;
+}

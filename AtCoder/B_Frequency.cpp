8ebafd61042c9f07f96be65f73d448c89e8b55a5
Noodle96 <jorgealfredomarino2@gmail.com>
2024-03-05 22:10:49 -0500

    B - Frequency

---
+++ AtCoder/B_Frequency.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+#define all(x) x.begin(),x.end()
+#define msg(str,str2) cout << str << str2<< endl
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+
+#define pb push_back
+#define mp make_pair
+
+int gcd(int a,int b){
+	if(a%b==0) return b;
+	else return gcd(b,a%b);
+}
+
+clock_t startTime;
+double getCurrentTime() {
+	return (double)(clock() - startTime) / CLOCKS_PER_SEC;
+}
+void solve(){
+	string s; cin >> s;
+	map<char,int> mapa;
+	for(int e = 0 ;e < (int)s.size() ; e++){
+		mapa[s[e]]++;
+	}
+	int maximo = 0;
+	char ch;
+	for(auto &[f,s]: mapa){
+		if(s > maximo){
+			maximo = s;
+			ch = f;
+		}
+	}
+	cout << ch << endl;
+
+}
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	#ifdef DEBUG
+		freopen("input.txt","r",stdin);
+		freopen("output.txt","w",stdout);
+	#endif
+	solve();
+	return 0;
+}

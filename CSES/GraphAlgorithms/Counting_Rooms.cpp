9a18bfe13e9a859b0b30b14b98f02e0f1c0a8a3b
Noodle96 <jorgealfredomarino2@gmail.com>
2024-03-13 18:06:19 -0500

    Counting Rooms

---
+++ CSES/GraphAlgorithms/Counting_Rooms.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+#define all(x) x.begin(),x.end()
+#define msg(str,str2) cout << str << str2<< endl
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+
+#define pb push_back
+#define mp make_pair
+
+int gcd(int a,int b){
+	if(a%b==0) return b;
+	else return gcd(b,a%b);
+}
+
+clock_t startTime;
+double getCurrentTime() {
+	return (double)(clock() - startTime) / CLOCKS_PER_SEC;
+}
+const int N = 1e3 +5;
+char grid[N][N];
+bool isVisited[N][N];
+int n,m,cont;
+int dx[] = {1,-1,0,0};
+int dy[] = {0,0,1,-1};
+
+bool isValidPosition(int x, int y){
+	return x >= 0 && x < n && y >= 0 && y < m;
+}
+
+
+void dfs(int x, int y){
+	if(isVisited[x][y]) return;
+	isVisited[x][y] = true;
+	for(int e = 0 ; e < 4; e++){
+		int new_x = x + dx[e];
+		int new_y = y + dy[e];
+		if(isValidPosition(new_x,new_y) && grid[new_x][new_y] == '.')
+			dfs(new_x,new_y);
+	}
+}
+void solve(){
+	cin>>n>>m;
+	for(int e = 0 ; e < n; e++)
+		for(int j = 0 ; j < m; j++){
+			cin>>grid[e][j];
+			isVisited[e][j] = false;
+		}
+	
+	cont = 0;
+	for(int e = 0 ; e < n; e++){
+		for(int j = 0 ; j < m; j++){
+			if(!isVisited[e][j] && grid[e][j] == '.'){
+				dfs(e,j);
+				cont++;
+			}
+		}
+	}
+	cout << cont << endl;
+}
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	#ifdef DEBUG
+		freopen("input.txt","r",stdin);
+		freopen("output.txt","w",stdout);
+	#endif
+	solve();
+	return 0;
+}

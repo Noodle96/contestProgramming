fef5be1003c7e28be944e432778b79895ad63c96
Noodle96 <jorgealfredomarino2@gmail.com>
2024-03-25 13:39:51 -0500

    A - Arithmetic Progression

---
+++ AtCoder/A_Arithmetic_Progression.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#define all(x) x.begin(),x.end()
+#define msg(str,str2) cout << str << str2<< endl
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+
+#define pb push_back
+#define mp make_pair
+
+int gcd(int a,int b){
+	if(a%b==0) return b;
+	else return gcd(b,a%b);
+}
+
+clock_t startTime;
+double getCurrentTime() {
+	return (double)(clock() - startTime) / CLOCKS_PER_SEC;
+}
+int a,b,d;
+void solve(){
+	cin>>a>>b>>d;
+	for(int e = a; e <=b; e+=d){
+		cout << e << " ";
+	}cout << endl;
+}
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	#ifdef DEBUG
+		freopen("input.txt","r",stdin);
+		freopen("output.txt","w",stdout);
+	#endif
+	solve();
+	return 0;
+}

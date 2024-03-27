85e718da8d0f5b27529380f366d337c0194e7517
Noodle96 <jorgealfredomarino2@gmail.com>
2024-03-27 13:11:34 -0500

    10696 - f91

---
+++ uva/05_Mathematics/Finding_Formula_Easier/f91.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#define all(x) x.begin(),x.end()
+#define msg(str,str2) cout << str << str2<< endl
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+
+#define pb push_back
+#define mp make_pair
+
+int gcd(int a,int b){
+	if(a%b==0) return b;
+	else return gcd(b,a%b);
+}
+
+clock_t startTime;
+double getCurrentTime() {
+	return (double)(clock() - startTime) / CLOCKS_PER_SEC;
+}
+int n;
+int f91(int n){
+	if(n>=101) return n-10;
+	return f91(f91(n+11));
+}
+void solve(){
+	while(cin>>n){
+		if(n == 0) break;
+		cout << "f91(" << n << ") = "<<f91(n) << endl;
+	}
+}
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	#ifdef DEBUG
+		freopen("input.txt","r",stdin);
+		freopen("output.txt","w",stdout);
+	#endif
+	solve();
+	return 0;
+}

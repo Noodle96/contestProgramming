d09e946caa5f6564bb12301755c997e67483bdb8
Noodle96 <jorgealfredomarino2@gmail.com>
2024-04-05 13:45:36 -0500

    A - Yay!

---
+++ AtCoder/A_Yay!.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+#define all(x) x.begin(),x.end()
+#define msg(str,str2) cout << str << str2<< endl
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+
+#define pb push_back
+#define mp make_pair
+
+int gcd(int a,int b){
+	if(a%b==0) return b;
+	else return gcd(b,a%b);
+}
+
+clock_t startTime;
+double getCurrentTime() {
+	return (double)(clock() - startTime) / CLOCKS_PER_SEC;
+}
+string s;
+void solve(){
+	cin>>s;
+	char same;
+	if(s[0] == s[1]){
+		same = s[0];
+	}else{
+		if(s[0] == s[2])same = s[0];
+		else same = s[1];
+	}
+	for(int e = 0 ; e < s.size(); e++){
+		if(s[e] != same){
+			cout << e+1 << endl;
+			break;
+		}
+	}
+}
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	#ifdef DEBUG
+		freopen("input.txt","r",stdin);
+		freopen("output.txt","w",stdout);
+	#endif
+	solve();
+	return 0;
+}

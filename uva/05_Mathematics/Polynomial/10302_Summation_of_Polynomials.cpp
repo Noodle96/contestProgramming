f5a77dd9629c454c6882adc8410cabb9c421b9f0
Noodle96 <jorgealfredomarino2@gmail.com>
2024-08-26 20:14:11 -0500

    10302 Sumation of polynomials Lev 1

---
+++ uva/05_Mathematics/Polynomial/10302_Summation_of_Polynomials.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#define all(x) x.begin(),x.end()
+#define msg(str,str2) cout << str << str2<< endl
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+
+#define pb push_back
+#define mp make_pair
+
+int gcd(int a,int b){
+    if(a%b==0) return b;
+    else return gcd(b,a%b);
+}
+
+clock_t startTime;
+double getCurrentTime() {
+    return (double)(clock() - startTime) / CLOCKS_PER_SEC;
+}
+ll Pow(ll x, ll y){
+    if(y == 0) return 1;
+    if(y&1) return x*Pow(x*x, y/2);
+    else return Pow(x*x, y/2);
+}
+void solve(ll n){
+    cout << Pow(n*(n+1)/2,2) << endl;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    #ifdef DEBUG
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+    ll x;
+    while(cin>>x) solve(x);
+    return 0;
+}

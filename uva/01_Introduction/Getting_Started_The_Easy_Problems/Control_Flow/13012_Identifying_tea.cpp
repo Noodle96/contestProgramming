37eab0f541bcfb039a444961db4e29ed543b8364
Noodle96 <jorgealfredomarino2@gmail.com>
2025-02-03 13:14:02 -0500

    13012	Identifying tea

---
+++ uva/01_Introduction/Getting_Started_The_Easy_Problems/Control_Flow/13012_Identifying_tea.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#define all(x) x.begin(),x.end()
+#define msg(str,str2) cout << str << str2<< endl
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+
+#define pb push_back
+#define mp make_pair
+
+ll gcd(ll a,ll b){
+    if(a%b==0) return b;
+    else return gcd(b,a%b);
+}
+
+clock_t startTime;
+double getCurrentTime() {
+    return (double)(clock() - startTime) / CLOCKS_PER_SEC;
+}
+
+void solve(){
+    int m,temp;
+    while(cin>>m){
+        int total = 0;
+        for(int e = 0 ; e < 5 ;e++){
+            cin>>temp;
+            if(temp == m) total++;
+        }
+        cout << total << endl;
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    #ifdef DEBUG
+        freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+    solve();
+    return 0;
+}

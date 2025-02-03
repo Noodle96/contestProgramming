98e3bdd293250923939c7b61b3481fc356aa2eea
Noodle96 <jorgealfredomarino2@gmail.com>
2025-02-03 13:22:55 -0500

    12279	Emoogle Balance

---
+++ uva/01_Introduction/Getting_Started_The_Easy_Problems/Control_Flow/12279_Emoogle_Balance.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+#define all(x) x.begin(),x.end()
+#define msg(str,str2) cout << str << str2<< endl
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+
+#define pb push_back
+#define mp make_pair
+
+ll gcd(ll a,ll b){
+    if(a%b==0) return b;
+    else return gcd(b,a%b);
+}
+
+clock_t startTime;
+double getCurrentTime() {
+    return (double)(clock() - startTime) / CLOCKS_PER_SEC;
+}
+int caso = 1;
+void solve(){
+    int n,temp;
+    while(cin>>n){
+        if(n != 0){
+            int ans = 0;
+            for(int e  = 0 ; e < n; e++){
+                cin>>temp;
+                if(temp > 0) ans++;
+                else ans--;
+            }
+            cout << "Case " << caso++ << ": " << ans << endl;
+        }
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    #ifdef DEBUG
+        freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+    solve();
+    return 0;
+}

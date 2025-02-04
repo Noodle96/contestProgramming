6c691cd80092f4e328ee5d16f6dcba1e88ba369d
Noodle96 <jorgealfredomarino2@gmail.com>
2025-02-04 17:21:08 -0500

    1585	Score

---
+++ uva/01_Introduction/Getting_Started_The_Easy_Problems/1D_Array_manipulation_Easier/1585_score.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+#define all(x) x.begin(),x.end()
+#define msg(str,str2) cout << str << str2<< endl
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+
+#define pb push_back
+#define mp make_pair
+
+ll gcd(ll a,ll b){
+    if(a%b==0) return b;
+    else return gcd(b,a%b);
+}
+
+clock_t startTime;
+double getCurrentTime() {
+    return (double)(clock() - startTime) / CLOCKS_PER_SEC;
+}
+
+void solve(){
+    string str; cin>>str;
+    int cont = 0, total = 0;
+    for(int e = 0 ; e < str.length(); e++){
+        if(str[e] == 'O'){
+            cont++;
+            total += cont;
+        }else{
+            cont = 0 ;
+        }
+    }
+    cout << total << endl;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    #ifdef DEBUG
+        freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+    int t; cin>>t;
+    while(t--)solve();
+    return 0;
+}

881ddedd7ca023d32394df92ae8d6f3ad340a3c7
Noodle96 <jorgealfredomarino2@gmail.com>
2025-04-16 19:38:06 -0500

    A_NIT_orz *bitmask *800

---
+++ CDFC/800/A_NIT_orz.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+#define all(x) x.begin(),x.end()
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+using vll = vector<ll>;
+
+#define pb push_back
+#define mp make_pair
+
+const ll INF = 1e18;
+
+ll gcd(ll a,ll b){
+    if(a%b==0) return b;
+    else return gcd(b,a%b);
+}
+
+clock_t startTime;
+double getCurrentTime() {
+    return (double)(clock() - startTime) / CLOCKS_PER_SEC;
+}
+
+void solve(){
+    int n;
+    ll z;
+    cin>>n>>z;
+    vector<ll> vec(n);
+    for(int e = 0 ; e < n; e++) cin>>vec[e];
+    ll ans = 0;
+    for(int e = 0 ; e < n; e++){
+        ans = max(ans, vec[e]|z);
+    }
+    cout << ans << endl;
+
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    #ifdef DEBUG
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+    int t; cin>>t;
+    while(t--) solve();
+    return 0;
+}

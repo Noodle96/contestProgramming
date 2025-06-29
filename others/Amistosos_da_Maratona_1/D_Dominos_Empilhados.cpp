3a8166706206732e2c378836d381d835702cb226
Noodle96 <jorgealfredomarino2@gmail.com>
2025-06-28 22:47:51 -0500

    D_ dominos empilados

---
+++ others/Amistosos_da_Maratona_1/D_Dominos_Empilhados.cpp
@@ -0,0 +1,56 @@
+/*
+==========================================================
+|  Archivo       : D_Dominos_Empilhados.cpp
+|  Autor         : Russell
+|  Fecha         : 2025-06-28 12:13
+|--------------------------------------------------------
+|  Tópicos utilizados:
+|  - implementation
+==========================================================
+*/
+
+#include <bits/stdc++.h>
+#define all(x) x.begin(),x.end()
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+using vll = vector<ll>;
+
+#define pb push_back
+#define mp make_pair
+
+const ll INF = 1e18;
+
+ll gcd(ll a,ll b){
+    if(a%b==0) return b;
+    else return gcd(b,a%b);
+}
+
+clock_t startTime;
+double getCurrentTime() {
+    return (double)(clock() - startTime) / CLOCKS_PER_SEC;
+}
+
+void solve(){
+    int n,m;
+    cin>>n>>m;
+    cout << n*m/2 << endl;
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    #ifdef DEBUG
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+    solve();
+    return 0;
+}

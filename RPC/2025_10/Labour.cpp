598f676f072b4c704b5a56d1de77cdc89888e1ea
Noodle96 <jorgealfredomarino2@gmail.com>
2025-09-08 19:21:39 -0500

    Add implementation of Labour.cpp for calculating minimum time based on input values

---
+++ RPC/2025_10/Labour.cpp
@@ -0,0 +1,58 @@
+/*
+==========================================================
+|  Archivo       : Labour.cpp
+|  Autor         : Russell
+|  Fecha         : 2025-09-06 15:04
+|--------------------------------------------------------
+|  Tópicos utilizados:
+|  - Implementation
+==========================================================
+*/
+
+#include <bits/stdc++.h>
+#define all(x) x.begin(),x.end()
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+using vll = vector<ll>;
+
+#define pb push_back
+#define mp make_pair
+
+const ll INF = 1e18;
+
+ll gcd(ll a,ll b){
+    if(a%b==0) return b;
+    else return gcd(b,a%b);
+}
+
+clock_t startTime;
+double getCurrentTime() {
+    return (double)(clock() - startTime) / CLOCKS_PER_SEC;
+}
+
+void solve(){
+    ll t; cin >> t;
+    ll b0   = max(0LL, t - 360);
+    ll b30  = max(30LL, t - 540);
+    ll b45  = max(45LL, t - 600);
+    cout << min({b0, b30, b45}) << endl;
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    #ifdef DEBUG
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+    solve();
+    return 0;
+}

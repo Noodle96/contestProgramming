c4de7709c593f54bce1af744b465f085d4a8a338
Noodle96 <jorgealfredomarino2@gmail.com>
2025-09-08 19:18:00 -0500

    Add implementation of A_Collatz_Conjecture.cpp for solving the Collatz conjecture problem

---
+++ CDFC/CDFC-D3/A_Collatz_Conjecture.cpp
@@ -0,0 +1,57 @@
+/*
+==========================================================
+|  Archivo       : A_Collatz_Conjecture.cpp
+|  Autor         : Russell
+|  Fecha         : 2025-09-07
+|--------------------------------------------------------
+|  Tópicos utilizados:
+==========================================================
+*/
+
+#include <bits/stdc++.h>
+#define all(x) x.begin(),x.end()
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+using vll = vector<ll>;
+
+#define pb push_back
+#define mp make_pair
+
+const ll INF = 1e18;
+
+ll gcd(ll a,ll b){
+    if(a%b==0) return b;
+    else return gcd(b,a%b);
+}
+
+clock_t startTime;
+double getCurrentTime() {
+    return (double)(clock() - startTime) / CLOCKS_PER_SEC;
+}
+
+void solve(){
+    ll k, x;
+    cin >> k >> x;
+    ll ans = x << k;
+    cout << ans << endl;
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    #ifdef DEBUG
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+    int t; cin >> t;
+    while(t--) solve();
+    return 0;
+}

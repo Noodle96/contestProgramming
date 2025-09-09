fc8d127afe7c79f92f66317ab533a263710790ea
Noodle96 <jorgealfredomarino2@gmail.com>
2025-09-08 19:20:19 -0500

    Add implementation of Farmers.cpp for calculating total cost of apples based on input values

---
+++ RPC/2025_10/Farmers.cpp
@@ -0,0 +1,60 @@
+/*
+==========================================================
+|  Archivo       : Farmers.cpp
+|  Autor         : Russell
+|  Fecha         : 2025-09-06
+|--------------------------------------------------------
+|  Tópicos utilizados:
+|  - Tema 1
+==========================================================
+*/
+
+#include <bits/stdc++.h>
+#define all(x) x.begin(),x.end()
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+using vll = vector<ll>;
+
+#define pb push_back
+#define mp make_pair
+
+const ll INF = 1e18;
+
+ll gcd(ll a,ll b){
+    if(a%b==0) return b;
+    else return gcd(b,a%b);
+}
+
+clock_t startTime;
+double getCurrentTime() {
+    return (double)(clock() - startTime) / CLOCKS_PER_SEC;
+}
+
+void solve(){
+    int a, b; cin >> a >> b;
+    int d; cin >> d;
+
+    int total_apples = a * b;
+    int dozens = (total_apples + 11) / 12;
+    int total_cost = dozens * d;
+    cout << total_cost << endl;
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    #ifdef DEBUG
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+    solve();
+    return 0;
+}

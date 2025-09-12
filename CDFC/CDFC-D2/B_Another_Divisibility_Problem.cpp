76c7844b662dc3dd37d29c94dcdd968f66e2d724
Noodle96 <jorgealfredomarino2@gmail.com>
2025-09-12 14:09:25 -0500

    Add implementation of B_Another_Divisibility_Problem.cpp for calculating the difference from a constant limit

---
+++ CDFC/CDFC-D2/B_Another_Divisibility_Problem.cpp
@@ -0,0 +1,62 @@
+/*
+==========================================================
+|  Archivo       : B_Another_Divisibility_Problem.cpp
+|  Autor         : Russell
+|  Fecha         : 2025-09-09
+|--------------------------------------------------------
+|  Tópicos utilizados:
+|  - math
+==========================================================
+*/
+
+#include <bits/stdc++.h>
+#define all(x) x.begin(),x.end()
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+using vll = vector<ll>;
+
+#define pb push_back
+#define mp make_pair
+
+const ll INF = 1e18;
+const ll LIM = 999999999LL; // 10^9 - 1
+
+ll gcd(ll a,ll b){
+    if(a%b==0) return b;
+    else return gcd(b,a%b);
+}
+
+clock_t startTime;
+double getCurrentTime() {
+    return (double)(clock() - startTime) / CLOCKS_PER_SEC;
+}
+
+void solve(){
+    ll x; cin >> x;
+    ll y = LIM - x;       // y = 999,999,999 - x
+    cout << y << '\n';
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    #ifdef DEBUG
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+    int t;
+    cin >> t;
+    // t = 1;
+    while(t--){
+        solve();
+    }
+    return 0;
+}

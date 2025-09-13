95771a7e35df763c2024c193f913aa998ed9be62
Noodle96 <jorgealfredomarino2@gmail.com>
2025-09-13 13:56:24 -0500

    Add implementation of B_Lasers.cpp

---
+++ CDFC/CDFC-D4/B_Lasers.cpp
@@ -0,0 +1,68 @@
+/*
+==========================================================
+|  Archivo       : B_Lasers.cpp
+|  Autor         : Russell
+|  Fecha         : 2025-09-13
+|--------------------------------------------------------
+|  Tópicos utilizados:
+|  - Implementation
+==========================================================
+*/
+
+#include <bits/stdc++.h>
+#define all(x) x.begin(),x.end()
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+using vll = vector<ll>;
+
+#define pb push_back
+#define mp make_pair
+
+const ll INF = 1e18;
+
+ll gcd(ll a,ll b){
+    if(a%b==0) return b;
+    else return gcd(b,a%b);
+}
+
+clock_t startTime;
+double getCurrentTime() {
+    return (double)(clock() - startTime) / CLOCKS_PER_SEC;
+}
+
+void solve(){
+    int n,m; cin >> n >> m;
+    int x,y; cin >> x >> y;
+    for(int e = 0 ; e< n; e++){
+        int n_i; cin >> n_i;
+    }
+    for(int e = 0 ; e< m; e++){
+        int m_i; cin >> m_i;
+    }
+    cout << n + m << endl;
+    
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    #ifdef DEBUG
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+    int t;
+    cin >> t;
+    // t = 1;
+    while(t--){
+        solve();
+    }
+    return 0;
+}

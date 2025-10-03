6ecc948ae74efd6a0d7928f0aa4163bb47fa5f48
Noodle96 <jorgealfredomarino2@gmail.com>
2025-10-03 15:07:49 -0500

    Add implementation of A_Increase_or_Smash.cpp for solving problems related to counting distinct elements

---
+++ CDFC/CDFC-D1+D2/A_Increase_or_Smash.cpp
@@ -0,0 +1,66 @@
+/*
+==========================================================
+|  Archivo       : A_Increase_or_Smash.cpp
+|  Autor         : Russell
+|  Fecha         : 2025-10-03 09:49
+|--------------------------------------------------------
+|  Tópicos utilizados:
+|  - implementation
+|  - div1 + div2
+==========================================================
+*/
+
+#include <bits/stdc++.h>
+#define all(x) x.begin(),x.end()
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+using vll = vector<ll>;
+
+#define pb push_back
+#define mp make_pair
+
+const ll INF = 1e18;
+
+ll gcd(ll a,ll b){
+    if(a%b==0) return b;
+    else return gcd(b,a%b);
+}
+
+clock_t startTime;
+double getCurrentTime() {
+    return (double)(clock() - startTime) / CLOCKS_PER_SEC;
+}
+
+void solve(){
+    int n; cin >> n;
+    map<int,int> cnt;
+    for(int i=0; i<n; i++){
+        int x; cin >> x;
+        cnt[x]++;
+    }
+    cout << cnt.size()*2 -1 << endl;
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    #ifdef DEBUG
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+    int t;
+    cin >> t;
+    // t = 1;
+    while(t--){
+        solve();
+    }
+    return 0;
+}

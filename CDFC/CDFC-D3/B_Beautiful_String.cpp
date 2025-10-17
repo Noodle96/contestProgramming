43405deef5bb448461cbccdd1a3fe5db0aa6b623
Noodle96 <jorgealfredomarino2@gmail.com>
2025-10-17 16:32:22 -0500

    Add implementation of B_Beautiful_String.cpp for solving problems related to implementation and div3

---
+++ CDFC/CDFC-D3/B_Beautiful_String.cpp
@@ -0,0 +1,79 @@
+/*
+==========================================================
+|  Archivo       : B_Beautiful_String.cpp
+|  Autor         : Russell
+|  Fecha         : 2025-10-17 16:24
+|--------------------------------------------------------
+|  Tópicos utilizados:
+|  - div3
+|  - constructive algorithms
+==========================================================
+*/
+
+#include <bits/stdc++.h>
+#define all(x) x.begin(),x.end()
+using namespace std;
+
+using ll = long long;
+using i64 = int64_t;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+using vll = vector<ll>;
+
+#define pb push_back
+#define mp make_pair
+
+const int INF = 1e9;
+
+ll gcd(ll a,ll b){
+    if(a%b==0) return b;
+    else return gcd(b,a%b);
+}
+ll floordiv(ll a, ll b) {
+    if (a >= 0) return a / b;
+    return - ( (-a + b - 1) / b );
+}
+ll ceildiv(ll a, ll b) {
+    // ceil(a/b) = -floor((-a)/b)
+    if (a >= 0) return (a + b - 1) / b;
+    return - ( (-a) / b );
+}
+
+clock_t startTime;
+double getCurrentTime() {
+    return (double)(clock() - startTime) / CLOCKS_PER_SEC;
+}
+
+void solve(){
+    int n; cin >> n;
+    string s; cin >> s;
+    vector<int> ones;
+    for(int i = 0 ; i < n ; i++){
+        if(s[i] == '1') ones.pb(i+1);
+    }
+    cout << ones.size() << "\n";
+    for(int i = 0 ; i < (int)ones.size() ; i++){
+        cout << ones[i] << " ";
+    }cout << endl;
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    #ifdef DEBUG
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+    int t;
+    cin >> t;
+    // t = 1;
+    while(t--){
+        solve();
+    }
+    return 0;
+}

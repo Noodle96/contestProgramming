144aee15a3bd63c8133a71451fe371185cf9127d
Noodle96 <jorgealfredomarino2@gmail.com>
2025-10-24 11:36:14 -0500

    Add implementation of A_Circle_of_Apple_Trees.cpp for solving problems related to implementation and div2

---
+++ CDFC/CDFC-D2/A_Circle_of_Apple_Trees.cpp
@@ -0,0 +1,66 @@
+/*
+==========================================================
+|  Archivo       : A_Circle_of_Apple_Trees.cpp
+|  Autor         : Russell
+|  Fecha         : 2025-10-10 09:40
+|--------------------------------------------------------
+|  Tópicos utilizados:
+|  - div2
+|  - implementation
+==========================================================
+*/
+
+#include <bits/stdc++.h>
+#define all(x) x.begin(),x.end()
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+using vll = vector<ll>;
+
+#define pb push_back
+#define mp make_pair
+
+const ll INF = 1e18;
+
+ll gcd(ll a,ll b){
+    if(a%b==0) return b;
+    else return gcd(b,a%b);
+}
+
+clock_t startTime;
+double getCurrentTime() {
+    return (double)(clock() - startTime) / CLOCKS_PER_SEC;
+}
+
+void solve(){
+    int n; cin >> n;
+    set<int> s;
+    for(int i = 0 ; i < n; i++){
+        int x; cin >> x;
+        s.insert(x);
+    }
+    cout << s.size() << endl;
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    #ifdef DEBUG
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+    int t;
+    cin >> t;
+    // t = 1;
+    while(t--){
+        solve();
+    }
+    return 0;
+}

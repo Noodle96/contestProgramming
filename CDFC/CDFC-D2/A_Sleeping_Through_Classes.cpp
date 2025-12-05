efb8e5e5fe9b370d52e7154e877dccea7361a126
Noodle96 <jorgealfredomarino2@gmail.com>
2025-12-05 11:34:29 -0500

    Add implementation of A_Sleeping_Through_Classes.cpp for solving a problem involving sleep patterns and awake intervals

---
+++ CDFC/CDFC-D2/A_Sleeping_Through_Classes.cpp
@@ -0,0 +1,97 @@
+/*
+==========================================================
+|  Archivo       : A_Sleeping_Thrugh_Classes.cpp
+|  Autor         : Russell
+|  Fecha         : 2025-12-05 09:35
+|--------------------------------------------------------
+|  Tópicos utilizados:
+|  - div2
+==========================================================
+*/
+
+#include <bits/stdc++.h>
+#define all(x) x.begin(),x.end()
+using namespace std;
+using ll = long long;
+using i64 = int64_t;
+using i32 = int32_t;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+using vll = vector<ll>;
+
+#define pb push_back
+#define mp make_pair
+
+const int INF = 1e9;
+
+ll gcd(ll a, ll b) {
+    if (a % b == 0) return b;
+    else return gcd(b, a % b);
+}
+
+ll floordiv(ll a, ll b) {
+    if (a >= 0) return a / b;
+    return - ( (-a + b - 1) / b );
+}
+
+ll ceildiv(ll a, ll b) {
+    // ceil(a/b) = -floor((-a)/b)
+    if (a >= 0) return (a + b - 1) / b;
+    return - ( (-a) / b );
+}
+
+int bitlen_unsigned(unsigned long long n) {
+    // __builtin_clzll: Cuenta los ceros a la izquierda
+    return (n == 0) ? 1 : 64 - __builtin_clzll(n);
+}
+
+clock_t startTime;
+double getCurrentTime() {
+    return (double)(clock() - startTime) / CLOCKS_PER_SEC;
+}
+
+void solve() {    
+    int n, k; cin >> n >> k;
+    string s; cin >> s;
+    s = " " + s;
+    vector<bool> awake(n + 1, false);
+    for (int i = 1; i <= n; ++i){
+        if (s[i] == '1'){
+            int R = min(n, i + k);
+            for (int j = i; j <= R; ++j){
+                awake[j] = true;
+            }
+        }
+    }
+
+    int sleepCount = 0;
+    for (int i = 1; i <= n; ++i){
+        if (!awake[i])
+            ++sleepCount;
+    }
+    cout << sleepCount << "\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    #ifdef DEBUG
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    int t;
+    cin >> t;
+    // t = 1;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}

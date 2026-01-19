b34b0a8f92b6da8d1226b110a914285169d96b00
Noodle96 <jorgealfredomarino2@gmail.com>
2026-01-19 11:42:42 -0500

    Add implementation of D_OutOfMemoryError.cpp for solving a problem involving memory management and value tracking

---
+++ CDFC/CDFC-D4/D_OutOfMemoryError.cpp
@@ -0,0 +1,115 @@
+/*
+==========================================================
+|  Archivo       : D_OutOfMemoryError.cpp
+|  Autor         : Russell
+|  Fecha         : 2026-01-18 10:44
+|--------------------------------------------------------
+|  Tópicos utilizados:
+|  - div4
+==========================================================
+*/
+
+#include <bits/stdc++.h>
+#define all(x) x.begin(),x.end()
+using namespace std;
+using ll = long long;
+using i64 = int64_t;
+using i32 = int32_t;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+using vll = vector<ll>;
+
+#define pb push_back
+#define mp make_pair
+
+const int INF = 1e9;
+
+ll gcd(ll a, ll b) {
+    if (a % b == 0) return b;
+    else return gcd(b, a % b);
+}
+
+ll floordiv(ll a, ll b) {
+    if (a >= 0) return a / b;
+    return - ( (-a + b - 1) / b );
+}
+
+ll ceildiv(ll a, ll b) {
+    // ceil(a/b) = -floor((-a)/b)
+    if (a >= 0) return (a + b - 1) / b;
+    return - ( (-a) / b );
+}
+
+int bitlen_unsigned(unsigned long long n) {
+    // __builtin_clzll: Cuenta los ceros a la izquierda
+    return (n == 0) ? 1 : 64 - __builtin_clzll(n);
+}
+
+clock_t startTime;
+double getCurrentTime() {
+    return (double)(clock() - startTime) / CLOCKS_PER_SEC;
+}
+
+void solve() {
+    int n, m;
+    ll h;
+    cin >> n >> m >> h;
+
+    vector<ll> base(n), cur(n);
+    vector<int> last(n, 0);
+
+    for (int i = 0; i < n; i++){
+        cin >> base[i];
+        cur[i] = base[i];
+    }
+
+    int timer = 1;
+
+    while (m--){
+        int b;
+        ll c;
+        cin >> b >> c;
+        b--;
+
+        if (last[b] != timer){
+            cur[b] = base[b];
+            last[b] = timer;
+        }
+        cur[b] += c;
+
+        if (cur[b] > h) {
+            timer++;
+        }
+    }
+    for (int i = 0; i < n; i++){
+        if (last[i] != timer)
+            cout << base[i] << " ";
+        else
+            cout << cur[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    #ifdef DEBUG
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    int t;
+    cin >> t;
+    // t = 1;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}

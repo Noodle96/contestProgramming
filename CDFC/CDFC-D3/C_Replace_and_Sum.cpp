95f54a5f5aa533f1848376dd8645e1ba66710ade
Noodle96 <jorgealfredomarino2@gmail.com>
2026-01-25 23:52:50 -0500

    Add implementation of C_Replace_and_Sum.cpp for solving a problem involving array replacement and sum queries

---
+++ CDFC/CDFC-D3/C_Replace_and_Sum.cpp
@@ -0,0 +1,98 @@
+/*
+==========================================================
+|  Archivo       : C_Replace_and_Sum.cpp
+|  Autor         : Russell
+|  Fecha         : 2026-01-25 23:37
+|--------------------------------------------------------
+|  Tópicos utilizados:
+|  - div3
+==========================================================
+*/
+
+#include <bits/stdc++.h>
+#define all(x) x.begin(),x.end()
+using namespace std;
+using ll = long long;
+using i64 = int64_t;
+using i32 = int32_t;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+using vll = vector<ll>;
+
+#define pb push_back
+#define mp make_pair
+
+const int INF = 1e9;
+
+ll gcd(ll a, ll b) {
+    if (a % b == 0) return b;
+    else return gcd(b, a % b);
+}
+
+ll floordiv(ll a, ll b) {
+    if (a >= 0) return a / b;
+    return - ( (-a + b - 1) / b );
+}
+
+ll ceildiv(ll a, ll b) {
+    // ceil(a/b) = -floor((-a)/b)
+    if (a >= 0) return (a + b - 1) / b;
+    return - ( (-a) / b );
+}
+
+int bitlen_unsigned(unsigned long long n) {
+    // __builtin_clzll: Cuenta los ceros a la izquierda
+    return (n == 0) ? 1 : 64 - __builtin_clzll(n);
+}
+
+clock_t startTime;
+double getCurrentTime() {
+    return (double)(clock() - startTime) / CLOCKS_PER_SEC;
+}
+
+void solve() {
+    int n,q; cin >> n >> q;
+    vector<int> a(n), b(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    for (int i = 0; i < n; i++) {
+        cin >> b[i];
+    }
+    a[n-1] = max(b[n-1], a[n-1]);
+    for(int i = n-2; i >= 0; i--){
+        a[i] = max(a[i], max(b[i], a[i + 1]));
+    }
+    vector<int> prefixSum(n + 1, 0);
+    for(int i = 1 ; i <= n ; i++){
+        prefixSum[i] = prefixSum[i - 1] + a[i - 1];
+    }
+    while(q--){
+        int l,r; cin >> l >> r;
+        cout << prefixSum[r] - prefixSum[l - 1] << " ";
+    }cout << "\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    #ifdef DEBUG
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    int t;
+    cin >> t;
+    // t = 1;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}

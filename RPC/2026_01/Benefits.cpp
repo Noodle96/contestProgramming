c486e8066e5a7c88010b9b2b821dfbfd93d9b178
Noodle96 <jorgealfredomarino2@gmail.com>
2026-02-23 11:36:10 -0500

    Add implementation of Benefits.cpp for solving a problem involving age and amount comparisons

---
+++ RPC/2026_01/Benefits.cpp
@@ -0,0 +1,91 @@
+/*
+==========================================================
+|  Archivo       : Benefits.cpp
+|  Autor         : Russell
+|  Fecha         : 2026-02-21 13:23
+|--------------------------------------------------------
+|  Tópicos utilizados:
+==========================================================
+*/
+
+#include <bits/stdc++.h>
+#define all(x) x.begin(),x.end()
+using namespace std;
+using ll = long long;
+using i64 = int64_t;
+using i32 = int32_t;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+using vll = vector<ll>;
+
+#define pb push_back
+#define mp make_pair
+
+const int INF = 1e9;
+
+ll gcd(ll a, ll b) {
+    if (a % b == 0) return b;
+    else return gcd(b, a % b);
+}
+
+ll floordiv(ll a, ll b) {
+    if (a >= 0) return a / b;
+    return - ( (-a + b - 1) / b );
+}
+
+ll ceildiv(ll a, ll b) {
+    // ceil(a/b) = -floor((-a)/b)
+    if (a >= 0) return (a + b - 1) / b;
+    return - ( (-a) / b );
+}
+
+int bitlen_unsigned(unsigned long long n) {
+    // __builtin_clzll: Cuenta los ceros a la izquierda
+    return (n == 0) ? 1 : 64 - __builtin_clzll(n);
+}
+
+clock_t startTime;
+double getCurrentTime() {
+    return (double)(clock() - startTime) / CLOCKS_PER_SEC;
+}
+
+void solve() {
+    int age1, amount1;
+    cin >> age1 >> amount1;
+    int age2, amount2;
+    cin >> age2 >> amount2;
+    int estimate;
+    cin >> estimate;
+    int total1 = (estimate - age1) * amount1;
+    int total2 = (estimate - age2)* amount2;
+    if(total1 >= total2){
+        cout << "1" << endl;
+    }
+    else{
+        cout << "2" << endl;
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    #ifdef DEBUG
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    int t;
+    // cin >> t;
+    t = 1;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}

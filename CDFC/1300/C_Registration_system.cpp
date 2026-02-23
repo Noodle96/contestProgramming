a7225b695004c545bf8f84b1c7f09455b0c088ce
Noodle96 <jorgealfredomarino2@gmail.com>
2026-02-23 12:42:26 -0500

    Add implementation of C_Registration_system.cpp for managing user registrations with unique name handling

---
+++ CDFC/1300/C_Registration_system.cpp
@@ -0,0 +1,95 @@
+/*
+==========================================================
+|  Archivo       : C_Registration_system.cpp
+|  Autor         : Russell
+|  Fecha         : 2026-02-23 12:04
+|--------------------------------------------------------
+|  Tópicos utilizados:
+|  - data structures: map
+|  - training
+|  - *1300
+==========================================================
+*/
+
+#include <bits/stdc++.h>
+#define all(x) x.begin(),x.end()
+using namespace std;
+using ll = long long;
+using i64 = int64_t;
+using i32 = int32_t;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+using vll = vector<ll>;
+
+#define pb push_back
+#define mp make_pair
+
+const int INF = 1e9;
+
+ll gcd(ll a, ll b) {
+    if (a % b == 0) return b;
+    else return gcd(b, a % b);
+}
+
+ll floordiv(ll a, ll b) {
+    if (a >= 0) return a / b;
+    return - ( (-a + b - 1) / b );
+}
+
+ll ceildiv(ll a, ll b) {
+    // ceil(a/b) = -floor((-a)/b)
+    if (a >= 0) return (a + b - 1) / b;
+    return - ( (-a) / b );
+}
+
+int bitlen_unsigned(unsigned long long n) {
+    // __builtin_clzll: Cuenta los ceros a la izquierda
+    return (n == 0) ? 1 : 64 - __builtin_clzll(n);
+}
+
+clock_t startTime;
+double getCurrentTime() {
+    return (double)(clock() - startTime) / CLOCKS_PER_SEC;
+}
+
+void solve() {
+    int n; cin >> n;
+    map<string, int> db;
+    while(n--){
+        string name; cin >> name;
+        auto it = db.find(name);
+        if(it != db.end()){
+            db[name]++;
+            string nw_name = name + to_string(db[name]);
+            cout << nw_name << endl;
+            db[nw_name] = 0;
+        }else{
+            cout << "OK" << endl;
+            db[name] = 0;
+        }
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    #ifdef DEBUG
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    int t;
+    // cin >> t;
+    t = 1;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}

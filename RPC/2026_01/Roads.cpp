365cf06254dc40614ee34b6d9109057932dc985e
Noodle96 <jorgealfredomarino2@gmail.com>
2026-02-23 11:37:13 -0500

    Add implementation of Roads.cpp for solving a problem involving union-find and minimum spanning trees

---
+++ RPC/2026_01/Roads.cpp
@@ -0,0 +1,144 @@
+/*
+==========================================================
+|  Archivo       : Roads.cpp
+|  Autor         : Russell
+|  Fecha         : 2026-02-21 14:41
+|--------------------------------------------------------
+|  Tópicos utilizados:
+|  - dfs
+==========================================================
+*/
+
+#include <bits/stdc++.h>
+#define all(x) x.begin(),x.end()
+using namespace std;
+using ll = long long;
+using i64 = int64_t;
+using i32 = int32_t;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+using vll = vector<ll>;
+using lii = tuple<ll, int, int>;
+
+#define pb push_back
+#define mp make_pair
+
+const int INF = 1e9;
+
+ll gcd(ll a, ll b) {
+    if (a % b == 0) return b;
+    else return gcd(b, a % b);
+}
+
+ll floordiv(ll a, ll b) {
+    if (a >= 0) return a / b;
+    return - ( (-a + b - 1) / b );
+}
+
+ll ceildiv(ll a, ll b) {
+    // ceil(a/b) = -floor((-a)/b)
+    if (a >= 0) return (a + b - 1) / b;
+    return - ( (-a) / b );
+}
+
+int bitlen_unsigned(unsigned long long n) {
+    // __builtin_clzll: Cuenta los ceros a la izquierda
+    return (n == 0) ? 1 : 64 - __builtin_clzll(n);
+}
+
+clock_t startTime;
+double getCurrentTime() {
+    return (double)(clock() - startTime) / CLOCKS_PER_SEC;
+}
+
+class UnionFind
+{ // OOP style
+private:
+    vector<int> p, rank, setSize; // vi p is the key part
+    int numSets;
+
+public:
+    UnionFind(int N)
+    {
+        p.assign(N, 0);
+        for (int i = 0; i < N; ++i)
+            p[i] = i;
+        rank.assign(N, 0);    // optional speedup
+        setSize.assign(N, 1); // optional feature
+        numSets = N;          // optional feature
+    }
+
+    int findSet(int i) { return (p[i] == i) ? i : (p[i] = findSet(p[i])); }
+    bool isSameSet(int i, int j) { return findSet(i) == findSet(j); }
+
+    int numDisjointSets() { return numSets; }            // optional
+    int sizeOfSet(int i) { return setSize[findSet(i)]; } // optional
+
+    void unionSet(int i, int j)
+    {
+        if (isSameSet(i, j))
+            return;                         // i and j are in same set
+        int x = findSet(i), y = findSet(j); // find both rep items
+        if (rank[x] > rank[y])
+            swap(x, y); // keep x 'shorter' than y
+        p[x] = y;       // set x under y
+        if (rank[x] == rank[y])
+            ++rank[y];            // optional speedup
+        setSize[y] += setSize[x]; // combine set sizes at y
+        --numSets;                // a union reduces numSets
+    }
+};
+
+void solve() {
+    int n,m,r;
+    cin >> n >> m >> r;
+    UnionFind uf(n);
+    for(int i = 0 ;i < m ; i++){
+        int a,b; cin >> a >> b;
+        a--, b--;
+        uf.unionSet(a,b);
+    }
+    vector<lii> E;
+    while(r--){
+        int a,b;
+        ll c;
+        cin >> a >> b >> c;
+        a--, b--;
+        E.emplace_back(c,a,b);
+    }
+    sort(all(E));
+    ll ans = 0;
+    for(int i = 0; i < E.size(); i++){
+        ll c;
+        int a,b;
+        tie(c,a,b) = E[i];
+        if(uf.isSameSet(a,b)) continue;
+        uf.unionSet(a,b);
+        ans += c;
+    }
+    cout << ans << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    #ifdef DEBUG
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    int t;
+    // cin >> t;
+    t = 1;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}

10256c7a9fed192615bf257a3d32a062cae6aa0b
Noodle96 <jorgealfredomarino2@gmail.com>
2026-02-25 18:22:16 -0500

    Add implementation of Factory_Machines.cpp for solving factory machine scheduling problems using binary search

---
+++ CSES/sortingAndSearching/Factory_Machines.cpp
@@ -0,0 +1,109 @@
+/*
+==========================================================
+|  Archivo       : Factory_Machines.cpp
+|  Autor         : Russell
+|  Fecha         : 2026-02-25 18:03
+|--------------------------------------------------------
+|  Tópicos utilizados:
+|  - binary search
+|  - training
+==========================================================
+*/
+
+#include <bits/stdc++.h>
+#define all(x) x.begin(),x.end()
+using namespace std;
+using ll = long long;
+using i64 = int64_t;
+using i32 = int32_t;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+using vll = vector<ll>;
+
+#define pb push_back
+#define mp make_pair
+
+const int INF = 1e9;
+
+ll gcd(ll a, ll b) {
+    if (a % b == 0) return b;
+    else return gcd(b, a % b);
+}
+
+ll floordiv(ll a, ll b) {
+    if (a >= 0) return a / b;
+    return - ( (-a + b - 1) / b );
+}
+
+ll ceildiv(ll a, ll b) {
+    // ceil(a/b) = -floor((-a)/b)
+    if (a >= 0) return (a + b - 1) / b;
+    return - ( (-a) / b );
+}
+
+int bitlen_unsigned(unsigned long long n) {
+    // __builtin_clzll: Cuenta los ceros a la izquierda
+    return (n == 0) ? 1 : 64 - __builtin_clzll(n);
+}
+
+clock_t startTime;
+double getCurrentTime() {
+    return (double)(clock() - startTime) / CLOCKS_PER_SEC;
+}
+
+void solve() {
+    int n;
+    ll t;
+    cin >> n >> t;
+    vector<ll> arr(n);
+    for(int i = 0 ;i < n ; i++){
+        cin >> arr[i];
+    }
+
+    auto can = [&](ll time) -> bool {
+        ll total = 0;
+        for(int i = 0;  i < n; i++){
+            total += time / arr[i];
+            if(total >= t) return true;
+        }
+        return false;
+    };
+
+    ll l = 0;
+    ll r = 1e18;
+    ll ans = -1;
+    while(l <= r){
+        ll mid = l + (r - l) / 2;
+        if(can(mid)){
+            ans = mid;
+            r = mid - 1;
+        }else{
+            l = mid + 1;
+        }
+    }
+    cout << ans << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    #ifdef DEBUG
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    int t;
+    // cin >> t;
+    t = 1;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}

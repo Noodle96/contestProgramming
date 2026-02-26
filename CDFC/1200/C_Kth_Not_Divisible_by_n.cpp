c9bbe2b6914e28a5c7ac9babec01c0e24fa5d2cc
Noodle96 <jorgealfredomarino2@gmail.com>
2026-02-26 11:24:46 -0500

    Add implementation of C_Kth_Not_Divisible_by_n.cpp for solving problems using binary search

---
+++ CDFC/1200/C_Kth_Not_Divisible_by_n.cpp
@@ -0,0 +1,105 @@
+/*
+==========================================================
+|  Archivo       : C_Kth_Not_Divisible_by_n.cpp
+|  Autor         : Russell
+|  Fecha         : 2026-02-26 11:17
+|--------------------------------------------------------
+|  Tópicos utilizados:
+|  - binary search
+|  - training
+|  - *1200
+==========================================================
+*/
+
+#include <bits/stdc++.h>
+#define all(x) x.begin(),x.end()
+using namespace std;
+using ll = long long;
+using i64 = int64_t;
+using i32 = int32_t;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+using vll = vector<ll>;
+
+#define pb push_back
+#define mp make_pair
+
+const int INF = 1e9;
+
+ll gcd(ll a, ll b) {
+    if (a % b == 0) return b;
+    else return gcd(b, a % b);
+}
+
+ll floordiv(ll a, ll b) {
+    if (a >= 0) return a / b;
+    return - ( (-a + b - 1) / b );
+}
+
+ll ceildiv(ll a, ll b) {
+    // ceil(a/b) = -floor((-a)/b)
+    if (a >= 0) return (a + b - 1) / b;
+    return - ( (-a) / b );
+}
+
+int bitlen_unsigned(unsigned long long n) {
+    // __builtin_clzll: Cuenta los ceros a la izquierda
+    return (n == 0) ? 1 : 64 - __builtin_clzll(n);
+}
+
+clock_t startTime;
+double getCurrentTime() {
+    return (double)(clock() - startTime) / CLOCKS_PER_SEC;
+}
+
+void solve() {
+    ll n, k;
+    cin >> n >> k;
+
+    auto can = [&](ll x) -> bool {
+        // Cantida de numero divisibles by n
+        ll div = x / n;
+        // cantidad de numeros no divisibles by n
+        ll no_div = x - div;
+        return no_div >= k; 
+    };
+
+    ll l = 0;
+    ll r = 1e18;
+    ll ans = -1;
+    while(l <= r){
+        ll mid = l + (r - l) / 2;
+        if(can(mid)){
+            ans = mid;
+            r = mid - 1;
+        }else{
+            l = mid  + 1;
+        }
+    }
+    cout << ans << endl;
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    #ifdef DEBUG
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    int t;
+    cin >> t;
+    // t = 1;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}

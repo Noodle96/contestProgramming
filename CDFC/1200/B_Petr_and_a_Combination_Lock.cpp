64362ab960bb535e236e1ff0776a18e2d995ae56
Noodle96 <jorgealfredomarino2@gmail.com>
2026-02-28 18:54:36 -0500

    Add implementation of B_Petr_and_a_Combination_Lock.cpp for solving combination lock problems using bitmasking

---
+++ CDFC/1200/B_Petr_and_a_Combination_Lock.cpp
@@ -0,0 +1,115 @@
+/*
+==========================================================
+|  Archivo       : B_Petr_and_a_Combination_Lock.cpp
+|  Autor         : Russell
+|  Fecha         : 2026-02-28 18:42
+|--------------------------------------------------------
+|  Tópicos utilizados:
+|  - bitmask
+|  - training
+|  - *1200
+==========================================================
+*/
+
+#include <bits/stdc++.h>
+#define all(x) x.begin(),x.end()
+using namespace std;
+using ll = long long;
+using i64 = int64_t;
+using i32 = int32_t;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+using vll = vector<ll>;
+
+#define pb push_back
+#define mp make_pair
+
+const int INF = 1e9;
+
+ll gcd(ll a, ll b) {
+    if (a % b == 0) return b;
+    else return gcd(b, a % b);
+}
+
+ll floordiv(ll a, ll b) {
+    if (a >= 0) return a / b;
+    return - ( (-a + b - 1) / b );
+}
+
+ll ceildiv(ll a, ll b) {
+    // ceil(a/b) = -floor((-a)/b)
+    if (a >= 0) return (a + b - 1) / b;
+    return - ( (-a) / b );
+}
+
+int bitlen_unsigned(unsigned long long n) {
+    // __builtin_clzll: Cuenta los ceros a la izquierda
+    return (n == 0) ? 1 : 64 - __builtin_clzll(n);
+}
+
+clock_t startTime;
+double getCurrentTime() {
+    return (double)(clock() - startTime) / CLOCKS_PER_SEC;
+}
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> angles(n);
+    for(int i = 0; i <  n; i++){
+        cin >> angles[i];
+    }
+
+    auto prove = [&](set<int> &idxs) -> int {
+        int ans = 0;
+        for(int i = 0 ; i < n; i++){
+            if(idxs.find(i) == idxs.end()){
+                ans -= angles[i];
+            }else{
+                ans += angles[i];
+            }
+        }
+        return ans;
+    };
+
+    // Formar todos los subsonjuntos = O(n * 2^n)
+    for(int mask = 0; mask < (1 << n); mask++){ 
+        set<int> idxs;
+        for(int i = 0 ; i < n; i++){
+            if(mask & (1 << i)){
+                idxs.insert(i);
+                // set idxs with counterclockwise
+                // the other counterclockwise
+                if(prove(idxs) % 360 == 0){
+                    cout << "YES\n";
+                    return;
+                }
+            }
+        }
+    }
+    cout << "NO\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    #ifdef DEBUG
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    int t;
+    // cin >> t;
+    t = 1;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}

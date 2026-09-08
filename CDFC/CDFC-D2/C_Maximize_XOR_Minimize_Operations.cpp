67dcc0d3b899d26c2e2f4f36747259311d6f8c5b
Noodle96 <jorgealfredomarino2@gmail.com>
2026-09-08 12:12:19 -0500

    Add implementation for C_Maximize_XOR_Minimize_Operations problem

---
+++ CDFC/CDFC-D2/C_Maximize_XOR_Minimize_Operations.cpp
@@ -0,0 +1,90 @@
+/*
+==========================================================
+|  Archivo       : C_Maximize_XOR_Minimize_Operations.cpp
+|  Autor         : Russell
+|  Fecha         : 2026-09-08
+|--------------------------------------------------------
+|  Tópicos utilizados:
+|  - contest
+==========================================================
+*/
+
+#include <bits/stdc++.h>
+#define all(x) x.begin(),x.end()
+using namespace std;
+using ll = long long;
+using i64 = int64_t;
+using i32 = int32_t;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+using vll = vector<ll>;
+
+#define pb push_back
+#define mp make_pair
+
+const int INF = 1e9;
+
+ll gcd(ll a, ll b) {
+    if (a % b == 0) return b;
+    else return gcd(b, a % b);
+}
+
+ll floordiv(ll a, ll b) {
+    if (a >= 0) return a / b;
+    return - ( (-a + b - 1) / b );
+}
+
+ll ceildiv(ll a, ll b) {
+    // ceil(a/b) = -floor((-a)/b)
+    if (a >= 0) return (a + b - 1) / b;
+    return - ( (-a) / b );
+}
+
+int bitlen_unsigned(unsigned long long n) {
+    // __builtin_clzll: Cuenta los ceros a la izquierda
+    return (n == 0) ? 1 : 64 - __builtin_clzll(n);
+}
+
+clock_t startTime;
+double getCurrentTime() {
+    return (double)(clock() - startTime) / CLOCKS_PER_SEC;
+}
+
+void solve() {
+    ll x, y;
+    cin >> x >> y;
+    ll s = x + y;
+    ll a = 0;
+    for (int bit = 32; bit >= 0; bit--) {
+        ll bitval = 1LL << bit;
+        if (s & bitval) {
+            if (a + bitval <= x) a += bitval;
+        }
+    }
+    ll ops = x - a;
+    cout << s << " " << ops << "\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    #ifdef DEBUG
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    int t;
+    cin >> t;
+    // t = 1;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
